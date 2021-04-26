1b703e07ca89be7a9034832f3abde36a59ca5875
Alphazene Solutions <marketingclientsofms@gmail.com>
2021-04-26 12:09:04 +0530

    Version 1.0

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cros("http://localhost:3000"))
 
 app.get("/createtable", (req, res) => {
-    var sql = "CREATE TABLE customersdata (version VARCHAR(255), status VARCHAR(255),date VARCHAR(255), author VARCHAR(255), owner VARCHAR(255), reviewed_by VARCHAR(255), reviewed_date VARCHAR(255), approver VARCHAR(255), approval_date VARCHAR(255), description VARCHAR(255))";
+    var sql = "CREATE TABLE customersdatas (date VARCHAR(255),shift VARCHAR(255),machine_Sl_No VARCHAR(255),checked_by VARCHAR(255),process1_status VARCHAR(255), process1_result VARCHAR(255),process2_status VARCHAR(255), process2_result VARCHAR(255),process3_status VARCHAR(255), process3_result VARCHAR(255),process4_status VARCHAR(255), process4_result VARCHAR(255),process5_status VARCHAR(255), process5_result VARCHAR(255),process6_status VARCHAR(255), process6_result VARCHAR(255),process7_status VARCHAR(255), process7_result VARCHAR(255),process8_status VARCHAR(255), process8_result VARCHAR(255),process9_status VARCHAR(255), process9_result VARCHAR(255),description VARCHAR(255))";
     db.query(sql, function (err, result) {
         if (err) {
             return res.send(err)
@@ -22,24 +22,9 @@ app.get("/createtable", (req, res) => {
 })
 
 app.post("/send", (req, res) => {
-    const { version, status, date, author, owner, reviewed_by, reviewed_date, approver, approval_date, description } = req.body    
-    var newdate = new Date();
-    var cdate = newdate.toLocaleString('en-US', { hour12: true })
-    const testvalues = {
-        version: "1.0",
-        status: status,
-        date: cdate,
-        author: "Nokia",
-        owner: "Nokia",
-        reviewed_by: reviewed_by,
-        reviewed_date: cdate,
-        approver: "Nokia",
-        approval_date: cdate,
-        description: description
-    }
-
-    const values=["1.0",status,cdate,"Nokia","Nokia",reviewed_by,cdate,"Nokia",cdate,description]
-    var sql = `INSERT INTO customersdata (version, status,date, author, owner, reviewed_by, reviewed_date, approver, approval_date, description) VALUES ('1.0','${status}','${cdate}','Nokia','Nokia','${reviewed_by}','${cdate}','Nokia','${cdate}','${description}')`;          
+    console.log(req.body)
+  const {date,shift,machine_Sl_No,checked_by,process1_status, process1_result,process2_status, process2_result,process3_status, process3_result,process4_status, process4_result,process5_status, process5_result,process6_status, process6_result,process7_status, process7_result,process8_status, process8_result,process9_status, process9_result,description}=req.body
+    var sql = `INSERT INTO customersdatas (date,shift,machine_Sl_No,checked_by,process1_status, process1_result,process2_status, process2_result,process3_status, process3_result,process4_status, process4_result,process5_status, process5_result,process6_status, process6_result,process7_status, process7_result,process8_status, process8_result,process9_status, process9_result,description) VALUES ('${date}','${shift}','${machine_Sl_No}','${checked_by}','${process1_status}', '${process1_result}','${process2_status}', '${process2_result}','${process3_status}', '${process3_result}','${process4_status}', '${process4_result}','${process5_status}', '${process5_result}','${process6_status}', '${process6_result}','${process7_status}', '${process7_result}','${process8_status}', '${process8_result}','${process9_status}', '${process9_result}','${description}')`;          
     db.query(sql, function (err, result) {
         if (err) {
             return res.send(err)
@@ -47,14 +32,15 @@ app.post("/send", (req, res) => {
             return res.send("Data Inserted")
         }
     });
+    
 
 })
 
 app.get("/getdatas",(req,res)=>{
-    db.query("SELECT * FROM customersdata", function (err, result, fields) {
+    db.query("SELECT * FROM customersdatas", function (err, result, fields) {
         if (err){
             return res.send(err)
-        }else{            
+        }else{                    
             return res.send(result)
         }
       });
